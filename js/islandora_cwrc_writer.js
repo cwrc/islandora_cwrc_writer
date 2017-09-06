f9bec6bc397acdddc2d23eda92b3cd2e1cf7d5bc
Andrew <ajmacdonald@users.noreply.github.com>
2017-09-06 19:39:24 -0400

    testing

--- js/islandora_cwrc_writer.js
+++ js/islandora_cwrc_writer.js
@@ -24,14 +24,10 @@ Drupal.CWRCWriter = Drupal.CWRCWriter || {};
           $('#cwrc_wrapper').height(1000);
         }
         
-        // Set the baseUrl, which will be used to load all the required javascript documents.
-        require.config({
-          baseUrl: settings.CWRCWriter.cwrcRootUrl + 'js',
-        });
-        
-        var Writer = require('cwrc-writer-base')
-
-        cwrcWriterInit.call(window, $, Writer);
+        var url = settings.CWRCWriter.cwrcRootUrl+'js/app.js';
+        $.getScript(url, function() {
+            cwrcWriterInit.call(window, $, Drupal.CWRCWriter);
+        })
 
         /*
         // Load required jQuery in noConflict mode.
@@ -126,15 +122,12 @@ function cwrcWriterInit($, Writer) {
   var writer, config;
   config = Drupal.settings.CWRCWriter;
   config.id = config.id || 'editor';
-  config.delegator = Delegator;
-  writer = new Writer(config);
+  
+  config.layout;
+  config.entityLookupDialogs;
+  
+  writer = Writer.writer;
   writer.init(config.id);
-  
-//  var config = require('./config')
-  //  config.storageDialogs = storageDialogs
-//  config.layout = require('./layout-config')
-//  config.entityLookupDialogs = require('cwrc-dialogs')
-  
 
   /**
    * Re-write the Delegator save to have schema info.
@@ -270,80 +263,7 @@ function cwrcWriterInit($, Writer) {
   Drupal.CWRCWriter.writer = writer;
   writer.event('writerInitialized').subscribe(function (writer) {
 
-    /*
-     * code cause the RDF Overlap dialog to appear twice - following codeblock
-     * removed as per (with details on how to implement properly 
-     * https://github.com/cwrc/CWRC-Writer/issues/420 - 2016-10-01
-     * code seems to account for a situation where the user would change 
-     * the schema through the settings dialog, which would require clearing 
-     * of the current document.
-     *
-     */
-/*
-    // When we change the schema we should update the document.
-    writer.event('schemaLoaded').subscribe(function () {
-      var defaultTEI =
-          '<TEI xmlns="http://www.tei-c.org/ns/1.0">' +
-          '<teiHeader>' +
-          '<fileDesc>' +
-          '<titleStmt>' +
-          '<title></title>' +
-          '</titleStmt>' +
-          '<publicationStmt>' +
-          '<p/>' +
-          '</publicationStmt>' +
-          '<sourceDesc>' +
-          '<p></p>' +
-          '</sourceDesc>' +
-          '</fileDesc>' +
-          '</teiHeader>' +
-          '<text>' +
-          '<body><p>Paste or type your text here</p></body>' +
-          '</text>' +
-          '</TEI>';
-      var defaultXML;
-      var root;
-      var defaultDoc;
-      var doc;
-      var parser = new DOMParser();
-      var content = writer.editor.getContent();
-      // Only reload content if there is actually content.
-      if (content === '') {
-        return;
-      }
-      // We have to parse as HTML as HTML entities will break XML parsing in
-      // firefox.
-      doc = parser.parseFromString(content, 'text/html');
-      // Embeds the document in the body tag.
-      root = doc.body.firstElementChild.getAttribute('_tag');
-      switch (writer.root) {
-      case 'TEI':
-        if (root !== 'TEI') {
-          defaultDoc = parser.parseFromString(defaultTEI, 'text/xml');
-          writer.converter.doProcessing(defaultDoc);
-        }
-        break;
-      default:
-        if (root !== writer.root) {
-          defaultXML = '<'+ writer.root + '></'+ writer.root + '>';
-          defaultDoc = parser.parseFromString(defaultXML, 'text/xml');
-          writer.converter.doProcessing(defaultDoc);
-        }
-        break;
-      }
-    });
-
-    */ 
-
-    // load modules then do the setup
-    require(['jquery', 'modules/entitiesList', 'modules/relations', 'modules/selection',
-      'modules/structureTree', 'modules/validation'],
-      function ($, EntitiesList, Relations, Selection, StructureTree, Validation) {
-        // The function setupLayoutAndModules() is provided by layout.js, this
-        // function assumes it was already loaded before it's called.
-        setupLayoutAndModules(writer, EntitiesList, Relations, Selection, StructureTree, Validation);
-        // Determine how to display.
-        if (typeof config.initial_mode !== 'undefined') {
+      if (typeof config.initial_mode !== 'undefined') {
           if (config.initial_mode == 'annotate') {
             writer.isAnnotator = true;
             writer.layout.open('west');
@@ -387,6 +307,5 @@ function cwrcWriterInit($, Writer) {
         if (window.location.hash) {
           writer.fileManager.loadInitialDocument(window.location.hash, config.schemaId);
         }
-      });
   });
 }
