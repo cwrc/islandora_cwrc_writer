5bec43341fc2ed7cdde0cadf063da8195f75c534
Andrew <ajmacdonald@users.noreply.github.com>
2017-11-14 13:31:08 -0500

    remove unused storage dialogs

--- js/app.js
+++ js/app.js
@@ -7,4 +7,3 @@ window.Drupal.CWRCWriterConfig = require('./config.js');
 window.Drupal.CWRCWriter = require('cwrc-writer-base');
 window.Drupal.CWRCWriterLayout = require('./layout-config.js');
 window.Drupal.CWRCWriterDialogs = require('cwrc-dialogs');
-window.Drupal.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
