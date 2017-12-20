8cd50116a6e222f9c685d6a5a3d2656f765a775e
Andrew <ajmacdonald@users.noreply.github.com>
2017-12-20 17:31:56 -0500

    remove jQuery from window

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 window.Drupal = window.Drupal || {};
-if (!window.$) {
-	window.jQuery = window.$ = require('jquery');
-}
 
 window.Drupal.CWRCWriterConfig = require('./config.js');
 window.Drupal.CWRCWriter = require('cwrc-writer-base');
 window.Drupal.CWRCWriterLayout = require('./layout-config.js');
+
 window.Drupal.CWRCWriterDialogs = require('cwrc-dialogs');
