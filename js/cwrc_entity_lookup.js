96b16f03b0f7c413a09fa15747b0aff717d86333
Andrew <ajmacdonald@users.noreply.github.com>
2018-05-08 11:15:30 -0400

    add credentials

--- js/cwrc_entity_lookup.js
+++ js/cwrc_entity_lookup.js
@@ -15,29 +15,31 @@ function setRootURL(url) {
 */
 function fetchWithTimeout(url, config = {}, timeout = 8000) {
 
-        return new Promise((resolve, reject) => {
-            // the reject on the promise in the timeout callback won't have any effect, *unless*
-            // the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
-            // the whole outer Promise, and the promise from the fetch is dropped entirely.
-            setTimeout(() => reject(new Error('Call to CWRC timed out')), timeout);
-            fetch(url, config).then(resolve, reject);
-        }).then(
-            response=>{
-                // check for ok status
-                if (response.ok) {
-                    return response.json()
-                }
-                // if status not ok, through an error
-                throw new Error(`Something wrong with the call to CWRC, possibly a problem with the network or the server. HTTP error: ${response.status}`);
-            }/*,
-            // instead of handling and rethrowing the error here, we just let it bubble through
-            error => {
-            // we could instead handle a reject from either of the fetch or setTimeout promises,
-            // whichever first rejects, do some loggingor something, and then throw a new rejection.
-                console.log(error)
-                return Promise.reject(new Error(`some error jjk: ${error}`))
-            }*/
-        )
+    config.credentials = 'include';
+
+    return new Promise((resolve, reject) => {
+        // the reject on the promise in the timeout callback won't have any effect, *unless*
+        // the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
+        // the whole outer Promise, and the promise from the fetch is dropped entirely.
+        setTimeout(() => reject(new Error('Call to CWRC timed out')), timeout);
+        fetch(url, config).then(resolve, reject);
+    }).then(
+        response=>{
+            // check for ok status
+            if (response.ok) {
+                return response.json()
+            }
+            // if status not ok, through an error
+            throw new Error(`Something wrong with the call to CWRC, possibly a problem with the network or the server. HTTP error: ${response.status}`);
+        }/*,
+        // instead of handling and rethrowing the error here, we just let it bubble through
+        error => {
+        // we could instead handle a reject from either of the fetch or setTimeout promises,
+        // whichever first rejects, do some loggingor something, and then throw a new rejection.
+            console.log(error)
+            return Promise.reject(new Error(`some error jjk: ${error}`))
+        }*/
+    )
 }
 
 // note that this method is exposed on the npm module to simplify testing,
