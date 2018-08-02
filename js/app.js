bf741a3ca589b083b06eeb05bfbbed504127a9d5
Andrew <ajmacdonald@users.noreply.github.com>
2018-08-02 11:18:28 -0400

    use new cwrc lookup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.Drupal = window.Drupal || {};
 
-let cwrc = require('cwrc-entity-lookup');
+let cwrc = require('cwrc-tei-entities-lookup');
 cwrc.setEntityRoot('https://commons.cwrc.ca');
 cwrc.setSearchRoot(Drupal.settings.CWRCWriter.cwrcDialogs.cwrcApiUrl);
 let viaf = require('viaf-entity-lookup');
@@ -10,6 +10,10 @@ let getty = require('getty-entity-lookup');
 let geonames = require('geonames-entity-lookup');
 
 window.Drupal.CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
+window.Drupal.CWRCWriterDialogs.showNoLinkButton(true);
+window.Drupal.CWRCWriterDialogs.showCreateNewButton(true);
+window.Drupal.CWRCWriterDialogs.setShowEditButton(true);
+window.Drupal.CWRCWriterDialogs.setEntityFormsRoot(Drupal.settings.CWRCWriter.cwrcDialogs.entityFormsRootUrl);
 window.Drupal.CWRCWriterDialogs.registerEntitySources({
     people: (new Map()).set('cwrc', cwrc).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
     places: (new Map()).set('cwrc', cwrc).set('geonames', geonames).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
