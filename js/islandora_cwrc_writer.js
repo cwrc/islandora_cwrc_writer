beb5517c3c33836aad3ccd335b35c4fdc2f2fa37
Andrew <ajmacdonald@users.noreply.github.com>
2018-09-25 13:39:38 -0400

    add code for removing entity collection cookie

--- js/islandora_cwrc_writer.js
+++ js/islandora_cwrc_writer.js
@@ -11,6 +11,85 @@
  * @param Delegator
  */
 Drupal.CWRCWriter = Drupal.CWRCWriter || {};
+
+// cookie code from https://github.com/carhartl/jquery-cookie/blob/master/src/jquery.cookie.js
+(function ($) {
+
+	var pluses = /\+/g;
+
+	function encode(s) { return config.raw ? s : encodeURIComponent(s); }
+
+	function decode(s) { return config.raw ? s : decodeURIComponent(s); }
+
+	function stringifyCookieValue(value) { return encode(config.json ? JSON.stringify(value) : String(value)); }
+
+	function parseCookieValue(s) {
+		if (s.indexOf('"') === 0) {
+			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+		}
+
+		try {
+			s = decodeURIComponent(s.replace(pluses, ' '));
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
+	}
+
+	var config = $.cookie = function (key, value, options) {
+		if (arguments.length > 1 && !$.isFunction(value)) {
+			options = $.extend({}, config.defaults, options);
+
+			if (typeof options.expires === 'number') {
+				var days = options.expires, t = options.expires = new Date();
+				t.setMilliseconds(t.getMilliseconds() + days * 864e+5);
+			}
+
+			return (document.cookie = [
+				encode(key), '=', stringifyCookieValue(value),
+				options.expires ? '; expires=' + options.expires.toUTCString() : '',
+				options.path    ? '; path=' + options.path : '',
+				options.domain  ? '; domain=' + options.domain : '',
+				options.secure  ? '; secure' : ''
+			].join(''));
+        }
+        
+		var result = key ? undefined : {},
+			cookies = document.cookie ? document.cookie.split('; ') : [],
+			i = 0,
+			l = cookies.length;
+
+		for (; i < l; i++) {
+			var parts = cookies[i].split('='),
+				name = decode(parts.shift()),
+				cookie = parts.join('=');
+
+			if (key === name) {
+				result = read(cookie, value);
+				break;
+			}
+
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
+			}
+		}
+
+		return result;
+	};
+
+	config.defaults = {};
+
+	$.removeCookie = function (key, options) {
+		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+		return !$.cookie(key);
+	};
+
+}(jQuery));
+
+
 (function ($) {
   'use strict';
   
@@ -93,6 +172,10 @@ Drupal.CWRCWriter = Drupal.CWRCWriter || {};
       
       writer = new Writer(config);
       
+      window.addEventListener('beforeunload', function() {
+        // remove cookie specifying which collection to save new entities to (which was added by cwrc-entity-management-forms)
+        $.removeCookie('cwrc-entity-collection')
+      });
       /**
        * Overwrite the writer.saveAndExit
        */
@@ -105,6 +188,9 @@ Drupal.CWRCWriter = Drupal.CWRCWriter || {};
               data: {'doc':docText, 'schema':writer.schemaManager.schemas[writer.schemaManager.schemaId]['pid']},
               success: function(data, status, xhr) {
                   writer.editor.isNotDirty = true;
+
+                  // remove cookie specifying which collection to save new entities to (which was added by cwrc-entity-management-forms)
+                  $.removeCookie('cwrc-entity-collection')
 
                   $.ajax({
                       url: Drupal.settings.basePath+'islandora/rest/v1/object/'+writer.currentDocId+'/lock',
